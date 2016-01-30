5e8746eb1ed19b5fe383b489f1f293a0a421d90e
Ryan Wholey <rjwholey@gmail.com>
2016-01-30 04:05:27 -0800

    remove build for now, may need later for Docker

--- build/server/routers/draftRouter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var router = require('express').Router();
-var controller = require('../controllers/draftController');
-
-router.get('/:leagueId', controller.startDraft);
-router.post('/:leagueId', controller.submitDraft);
-
-module.exports = router;
